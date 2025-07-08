69dddb420f77b3dd2c54531721b914dc314caf6c
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2025-07-08 21:32:54 +0700

    :+1: Implement deploy frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import axios from '@/config/axios';
+import { ref, onMounted } from 'vue';
+const users = ref([])
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('/api/users')
+    users.value = response.data
+  } catch (error) {
+    console.error('Lỗi gọi API:', error)
+  }
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <ul>
+    <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+  </ul>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
