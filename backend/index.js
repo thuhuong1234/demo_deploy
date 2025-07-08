6e6dc80ab8377abc17e39166575ad4477912cb93
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2025-07-08 19:39:54 +0700

    :+1: Add router users

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,17 @@ const dotenv = require("dotenv");
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.get("/api/users", (req, res) => {
+  res.json([
+    { id: 1, name: "Nguyen Van A" },
+    { id: 2, name: "Le Thi B" },
+  ]);
 });
 
 app.listen(port, () => {
